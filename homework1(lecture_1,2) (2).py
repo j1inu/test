ac81576e9958d7c3408ff49c1b85e71816b83fdb
j1inu <jinuc0104@gmail.com>
2021-08-10 12:44:26 +0900

    첫 수업숙제

--- homework1(lecture_1,2) (2).py
+++ homework1(lecture_1,2) (2).py
@@ -31,60 +31,69 @@ print(138+234)
 print(684+2878)
 ## 문자열연산 homework2
 ## : 문자열 곱셈을 활용하여 같은 문장이 10회 반복되는 문장을 출력해보자
-
+string = 'Hello'
+print(string*10)
 
 ## 복합할당연산자 homework
 ## : 아래 복합할당연산자를 풀어쓴 의미와 식의 답을 그 줄의 주석으로 작성해보자.
 ## (계산 결과는 프로그램 실행을 통해 확인 후 작성해도 상관 없음.)
 '''
-number = 10     # 
-number += 15    # 
-number *= 3     # 
-number /= 7     # 
-number -= 100   # 
+number = 10     # 10
+number += 15    # 25
+number *= 3     # 30
+number /= 7     # 1.4285714285714286
+number -= 100   # -90
 '''
 
 # [연산(2) homework]
 ## 비교연산 homework
 ## : 아래 비교연산의 결과 예상하여 주석으로 작성해보고 연산결과와 비교해보자
 '''
-print(15>7)             #
-print(163<199)          #
-print(1>=2)             #
-print(7660>=7660)       #
-print(5==5)             #
-print(700!=700)         #
-print("ijijjjjjijjiiiiji" == "ijijjjjjjijjiiiiji")      #
-print("illililillllllilil" != "illililillllllilil")     #
+print(15>7)             #True
+print(163<199)          #True
+print(1>=2)             #False
+print(7660>=7660)       #True
+print(5==5)             #True
+print(700!=700)         #False
+print("ijijjjjjijjiiiiji" == "ijijjjjjjijjiiiiji")      #False
+print("illililillllllilil" != "illililillllllilil")     #False
 '''
 
 ## 논리연산 homework
 ## : 아래 논리연산의 결과 예상하여 주석으로 작성해보고 연산결과와 비교해보자
 '''
-print(75 <100 and 37 >= 40)     #       
-print("따-따-따-따, 따따다 따따따!" == "따-따-따-따- 물보라를 일으켜~" or "크크루크크" == "크크루삥뽕")     #
-print(not 5813==8446)           #
+print(75 <100 and 37 >= 40)     #    False   
+print("따-따-따-따, 따따다 따따따!" == "따-따-따-따- 물보라를 일으켜~" or "크크루크크" == "크크루삥뽕")     #  False or True
+print(not 5813==8446)           #    True
 '''
 
 ## 멤버십연산 homework
 ## : 아래 멤버십연산의 결과 예상하여 주석으로 작성해보고 연산결과와 비교해보자
 '''
-print("a" in "angry")      # 
-print("b" not in "bird")   # 
+print("a" in "angry")      # True
+print("b" not in "bird")   # False
 '''
 
 # [입출력과 자료형 변환 homework]
 ## 입출력과 자료형 변환 Mission3(자료2 ppt 32p 참고)
 ## : 출생년도를 입력하고 나이를 출력해보자.
-
-
+age = int(input("출생년도를 입력하세요 >>"))
+result = 2022-age
+print(result)
 # [조건문 homework]
 ## 조건문 Mission3(자료2 ppt 41p)
 ## : 국어, 영어, 수학 점수를 입력받고 합계와 평균를 출력한 뒤.
 ## - 평균이 60점이 넘을 경우: "보충 대상자가 아닙니다. 즐거운 방학보내세요"
 ## - 평균이 60점이 넘지 않을 경우: "보충 대상자입니다. 당신의 방학은 이제 제껍니다 ㅎㅎ"
 ## 를 출력하는 프로그램을 만들어 보자
-
+literature = int(input("국어 점수를 입력하세요 >> "))
+english = int(input("영어 점수를 입력하세요 >> "))
+math = int(input("수학 점수를 입력하세요 >> "))
+result = literature+english+math
+if result > 180:
+    print("보충 대상자가 아닙니다. 즐거운 방학보내세요")
+else:
+    print("보충 대상자입니다. 당신의 방학은 이제 제껍니다 ㅎㅎ")
 
 
 ## 조건문 Mission4(자료2 ppt 43p)
