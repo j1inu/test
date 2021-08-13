4f1ef886a707193971a916ec23db2fca63879991
j1inu <jinuc0104@gmail.com>
2021-08-13 15:09:34 +0900

    2회차 수업숙제

--- homework1(lecture_1,2) (2).py
+++ homework1(lecture_1,2) (2).py
@@ -63,7 +63,7 @@ print("illililillllllilil" != "illililillllllilil")     #False
 ## : 아래 논리연산의 결과 예상하여 주석으로 작성해보고 연산결과와 비교해보자
 '''
 print(75 <100 and 37 >= 40)     #    False   
-print("따-따-따-따, 따따다 따따따!" == "따-따-따-따- 물보라를 일으켜~" or "크크루크크" == "크크루삥뽕")     #  False or True
+print("따-따-따-따, 따따다 따따따!" == "따-따-따-따- 물보라를 일으켜~" or "크크루크크" == "크크루삥뽕")     #  False or True 
 print(not 5813==8446)           #    True
 '''
 
@@ -77,29 +77,34 @@ print("b" not in "bird")   # False
 # [입출력과 자료형 변환 homework]
 ## 입출력과 자료형 변환 Mission3(자료2 ppt 32p 참고)
 ## : 출생년도를 입력하고 나이를 출력해보자.
+'''
 age = int(input("출생년도를 입력하세요 >>"))
-result = 2022-age
+result = 2021-age+1
 print(result)
+'''
+
 # [조건문 homework]
 ## 조건문 Mission3(자료2 ppt 41p)
 ## : 국어, 영어, 수학 점수를 입력받고 합계와 평균를 출력한 뒤.
 ## - 평균이 60점이 넘을 경우: "보충 대상자가 아닙니다. 즐거운 방학보내세요"
 ## - 평균이 60점이 넘지 않을 경우: "보충 대상자입니다. 당신의 방학은 이제 제껍니다 ㅎㅎ"
 ## 를 출력하는 프로그램을 만들어 보자
+
+'''
 literature = int(input("국어 점수를 입력하세요 >> "))
 english = int(input("영어 점수를 입력하세요 >> "))
 math = int(input("수학 점수를 입력하세요 >> "))
-result = literature+english+math
+result = literature + english + math
 if result > 180:
     print("보충 대상자가 아닙니다. 즐거운 방학보내세요")
 else:
     print("보충 대상자입니다. 당신의 방학은 이제 제껍니다 ㅎㅎ")
-
+'''
 
 ## 조건문 Mission4(자료2 ppt 43p)
 ## : Up-down 게임 만들기
 ## random 함수를 활용하여 1~100까지의 숫자를 생성하고 up-down 게임 만들어보기
-'''
+
 import random
 
 print("[Up-Down Game]")
@@ -109,14 +114,24 @@ while True:
     # random_num과 비교하여 '정답/Up/Down'을 출력하는 코드 작성
     # 아래 코드에 내용 추가하기 
 
-    your_num = 
-    if :
+    your_num = int(input("숫자입력>>>"))
+    if your_num == random_num:
         print("정답입니다~~!")
         break
-    elif :
+    elif your_num > random_num:
         print("Down")
     else:
         print("Up")
-'''
+
+
+
+
+
+
+
+
+
+
+
 
 
